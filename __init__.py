e423c9f53654e42a052e4c55c192e9b35b893fc8
Alex <alex.apr9@gmail.com>
2019-04-03 12:14:44 +0200

    Autoload added

--- __init__.py
+++ __init__.py
@@ -4,9 +4,29 @@ Created on 3 Jan 2017
 @author: Alex
 '''
 
+bl_info = {
+    "name" : "UnifiedTool",
+    "author" : "Alexcode9",
+    "description" : "",
+    "blender" : (2, 80, 0),
+    "location" : "",
+    "warning" : "",
+    "category" : "Generic"
+}
+
 import bpy
 import time, sys
 from bpy.props import *
+
+from . import auto_load
+
+auto_load.init()
+
+def register():
+    auto_load.register()
+
+def unregister():
+    auto_load.unregister()
 
 
 def update_progress(job_title, progress):
